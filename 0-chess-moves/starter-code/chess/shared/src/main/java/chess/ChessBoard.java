1f85acd460a58260ab1e188fa73f30ea05e8f397
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-01-10 20:49:57 -0700

    Planning and starting to build gameboard

--- 0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessBoard.java
+++ 0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import javax.print.CancelablePrintJob;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +10,12 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece [][] board;
     public ChessBoard() {
-        
+        this.board = new ChessPiece[8][8];
+
+        resetBoard();
+        //Maybe call resetBoard() here to make it the default board.
     }
 
     /**
@@ -19,7 +25,9 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        //Include error handling
+        // -- out of bounds handling could also be handled when constructing position.
+        this.board[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +38,10 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        //What error handling is needed?
+        //Make sure the position is on the board?
+        //Does color or type matter?
+        return this.board[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +49,78 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        //Start by looping through row 1 and row 6 to set pawns.
+        for (int col = 0; col < 8; col++)
+        {
+            //Top Row
+            ChessPosition top = new ChessPosition(1, col);
+            ChessPiece topPawn = new ChessPiece(Color, ChessPiece.PieceType.PAWN); // Need to decide how we choose colors
+            this.addPiece(top, topPawn);
+
+            //Bottom Row
+            ChessPosition bottom = new ChessPosition(6, col);
+            ChessPiece bottomPawn = new ChessPiece(OtherColor, ChessPiece.PieceType.PAWN); //Whatever the other color is
+            this.addPiece(bottom, bottomPawn);
+        }
+        //Set Rooks
+        ChessPosition topLeft = new ChessPosition(0,0);
+        ChessPosition topRight = new ChessPosition(0,7);
+        ChessPiece topLeftRook = new ChessPiece(Color, ChessPiece.PieceType.ROOK);
+        ChessPiece topRightRook = new ChessPiece(Color, ChessPiece.PieceType.ROOK);
+        this.addPiece(topLeft, topLeftRook);
+        this.addPiece(topRight, topRightRook);
+
+        ChessPosition bottomLeft = new ChessPosition(7,0);
+        ChessPosition bottomRight = new ChessPosition(7,7);
+        ChessPiece bottomLeftRook = new ChessPiece(OtherColor, ChessPiece.PieceType.ROOK);
+        ChessPiece bottomRightRook = new ChessPiece(OtherColor, ChessPiece.PieceType.ROOK);
+        this.addPiece(bottomLeft, bottomLeftRook);
+        this.addPiece(bottomRight, bottomRightRook);
+
+        //Set Knights
+        ChessPosition topLeftK = new ChessPosition(0,1);
+        ChessPosition topRightK = new ChessPosition(0,6);
+        ChessPiece topLeftKnight = new ChessPiece(Color, ChessPiece.PieceType.KNIGHT);
+        ChessPiece topRightKnight = new ChessPiece(Color, ChessPiece.PieceType.KNIGHT);
+        this.addPiece(topLeftK, topLeftKnight);
+        this.addPiece(topRightK, topRightKnight);
+
+        ChessPosition bottomLeftK = new ChessPosition(7,1);
+        ChessPosition bottomRightK = new ChessPosition(7,6);
+        ChessPiece bottomLeftKnight = new ChessPiece(OtherColor, ChessPiece.PieceType.KNIGHT);
+        ChessPiece bottomRightKnight = new ChessPiece(OtherColor, ChessPiece.PieceType.KNIGHT);
+        this.addPiece(bottomLeftK, bottomLeftKnight);
+        this.addPiece(bottomRightK, bottomRightKnight);
+
+        //Set Bishops
+        ChessPosition topLeftB = new ChessPosition(0,2);
+        ChessPosition topRightB = new ChessPosition(0,5);
+        ChessPiece topLeftBishop = new ChessPiece(Color, ChessPiece.PieceType.BISHOP);
+        ChessPiece topRightBishop = new ChessPiece(Color, ChessPiece.PieceType.BISHOP);
+        this.addPiece(topLeftB, topLeftBishop);
+        this.addPiece(topRightB, topRightBishop);
+
+        ChessPosition bottomLeftB = new ChessPosition(7,2);
+        ChessPosition bottomRightB = new ChessPosition(7,5);
+        ChessPiece bottomLeftBishop = new ChessPiece(OtherColor, ChessPiece.PieceType.BISHOP);
+        ChessPiece bottomRightBishop = new ChessPiece(OtherColor, ChessPiece.PieceType.BISHOP);
+        this.addPiece(bottomLeftB, bottomLeftBishop);
+        this.addPiece(bottomRightB, bottomRightBishop);
+
+        //Set Queens
+        ChessPosition topQueenPos = new ChessPosition(0,3);
+        ChessPosition bottomQueenPos = new ChessPosition(7,3);
+        ChessPiece topQueen = new ChessPiece(Color, ChessPiece.PieceType.QUEEN);
+        ChessPiece bottomQueen = new ChessPiece(Color, ChessPiece.PieceType.QUEEN);
+        this.addPiece(topQueenPos, topQueen);
+        this.addPiece(bottomQueenPos, bottomQueen);
+
+        //Set Kings
+        ChessPosition topKingPos = new ChessPosition(0,4);
+        ChessPosition bottomKingPos = new ChessPosition(7,4);
+        ChessPiece topKing = new ChessPiece(Color, ChessPiece.PieceType.KING);
+        ChessPiece bottomKing = new ChessPiece(Color, ChessPiece.PieceType.KING);
+        this.addPiece(topKingPos, topKing);
+        this.addPiece(bottomKingPos, bottomKing);
     }
 }
