716924f16b8098da3043556ac26939ade2dd88e9
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-02-24 11:19:26 -0700

    Finished Implementing LogoutService

--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,9 +1,16 @@
 package service;
 
+import dataAccess.DataAccessException;
+import dataAccess.MemoryAuthDAO;
+
 public class LogoutService {
-    void logout(LogoutRequest r) {
+    public void logout(LogoutRequest r) throws DataAccessException {
         //verify the authToken
         //Delete the authToken from the database
-
+        MemoryAuthDAO memoryAuthDAO = new MemoryAuthDAO();
+        if (memoryAuthDAO.verifyAuth(r.authToken()) == null) { //If the AuthData object returns null. Throw unauthorized error
+            throw new DataAccessException("Error: unauthorized");
+        }
+        memoryAuthDAO.deleteAuth(r.authToken());
     }
 }
