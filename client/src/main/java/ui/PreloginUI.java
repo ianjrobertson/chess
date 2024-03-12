68d07788377c6c7cfb607162b6d34a0f3d2fced5
ianjrobertson <45133404+ianjrobertson@users.noreply.github.com>
2024-03-11 20:05:10 -0600

    Begining to implement PreloginUI.java

---
+++ client/src/main/java/ui/PreloginUI.java
@@ -0,0 +1,82 @@
+package ui;
+
+import java.util.Scanner;
+
+public class PreloginUI {
+
+    //Read, eval, Print, loop.
+
+    private boolean running;
+
+    public PreloginUI() {
+        running = true;
+    }
+
+    public void run() {
+        System.out.println("Type \"help\" to get started");
+        Scanner scanner = new Scanner(System.in);
+        while (running) {
+            String input = scanner.nextLine();
+            String command = getFirstWord(input);
+            switch(command) {
+                case("help") -> this.help();
+                case("quit") -> this.quit();
+                case("login") -> this.login(input);
+                case("register") -> this.register(input);
+                case null, default -> this.unknownInput();
+            }
+        }
+        scanner.close();
+    }
+
+    public static String getFirstWord(String line) {
+        String[] words = line.trim().split("\\s+");
+        if (words.length > 0) {
+            return words[0];
+        } else {
+            return ""; // or throw an exception, depending on your requirements
+        }
+    }
+
+    private void unknownInput() {
+        System.out.println("Unknown input, please try again");
+    }
+
+    /**
+     * Displays text letting the user know what options they can take
+     */
+    private void help() {
+        System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+        System.out.println("login <USERNAME> <PASSWORD> - to play chess");
+        System.out.println("quit - exit game");
+        System.out.println("help - list options");
+    }
+
+
+
+    private void quit() {
+        running = false;
+        System.out.println("Have a nice day! :)");
+    }
+
+    private void login(String input) {
+        String[] words = input.trim().split("\\s+");
+        String username = words[1];
+        String password = words[2];
+
+        System.out.println(username + " " + password);
+
+        //Have a server facade class. Call the login method on it with a login request object
+    }
+
+    private void register(String input) {
+        String[] words = input.trim().split("\\s+");
+        String username = words[1];
+        String password = words[2];
+        String email = words[3];
+
+        System.out.println(username + " " + password + " " + email);
+        // have a server facade class, call the register method on it with a register request.
+
+    }
+}
